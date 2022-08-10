c9346378613f50432a9e4129a493078e8dbcfb47
KarlaRFrias <64796406+KarlaRFrias@users.noreply.github.com>
2022-08-10 16:53:06 -0500

    Se agrega cambio para probar pull

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,5 @@ print("Segundo commit")
 print("esto solo aparece en la rama de desarrollo")
 
 print("este cambio es para ver diferencias en archivos")
+
+print("Este cambio es para probar la función de pull")
